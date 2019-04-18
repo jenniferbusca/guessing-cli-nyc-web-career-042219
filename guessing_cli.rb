89ad606b335cfcab5b978138beedab44a0170d3a
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-18 20:52:55 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
 	  puts "Guess a number between 1 and 6."
 	  comp = rand(1..6)
     answer = gets.chomp
-    if answer == comp
+    if answer.to_i == comp
       puts "You guessed the correct number!"
     elsif 
       puts "The computer guessed #{comp}."
