0d0caab0fa70f1c429a8e0895df4dd93666b836c
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-18 20:36:30 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,2 +1,10 @@
 def run_guessing_game
+  loop do
+	  puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+  end
+  puts "You passed the quiz!"
+end
+
 end
