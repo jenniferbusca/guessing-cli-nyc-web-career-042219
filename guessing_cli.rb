b1b2009e9215533b52cf29aabc3b49a0bf12ac53
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-18 20:39:47 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,7 @@ def run_guessing_game
   loop do
 	  puts "Guess a number between 1 and 6."
     answer = gets.chomp
+    if answer == 
     break if answer == "Wingardium Leviosa"
   end
   puts "You passed the quiz!"
